2bc06102c5a059833324487bc1527c18abdddb4a
Vera <116074633+SlivinskayaVera@users.noreply.github.com>
2024-06-24 15:52:57 +0300

    Delete src/components/SVGImage directory

--- src/components/SVGImage/SVGImage.tsx
+++
@@ -1,9 +0,0 @@
-type SVGPropsType = { url: string; className: string };
-
-export function SVG({ url, className }: SVGPropsType) {
-  return (
-    <svg className={className}>
-      <use xlinkHref={`icon/sprite.svg#icon-${url}`}></use>
-    </svg>
-  );
-}
